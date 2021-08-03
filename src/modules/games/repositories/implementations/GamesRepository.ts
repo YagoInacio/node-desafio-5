c4f93ebede644c90be9a80343f68ccadd3169701
Yago Inacio <yagofaran@gmail.com>
2021-08-03 15:06:42 -0300

    created queries to select required data

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -2,7 +2,6 @@ import { getRepository, Repository } from 'typeorm';
 
 import { User } from '../../../users/entities/User';
 import { Game } from '../../entities/Game';
-
 import { IGamesRepository } from '../IGamesRepository';
 
 export class GamesRepository implements IGamesRepository {
@@ -15,16 +14,25 @@ export class GamesRepository implements IGamesRepository {
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
       .createQueryBuilder()
-      // Complete usando query builder
+      .select()
+      .where('title ilike :title', { title: `%${param}%` })
+      .getMany();
+    // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      select count(*) from games
+    `); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('games')
+      .leftJoin('games.users', 'users')
+      .where('games.id = :id', { id })
+      .select('users.*')
+      .getRawMany();
+    // Complete usando query builder
   }
 }
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,26 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({ relations: ['games'], where: { id: user_id } });
+
+    return user as User;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      select * from users 
+      order by first_name
+    `); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      select * from users
+      where first_name ilike '${first_name}'
+      and last_name ilike '${last_name}'
+    `); // Complete usando raw query
   }
 }
